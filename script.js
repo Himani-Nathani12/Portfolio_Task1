2b95d5b10f5d745e269eeb4d164aed70ab388468
Himani Nathani <131337924+Himani-Nathani12@users.noreply.github.com>
2024-05-20 05:39:44 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,42 @@
+function updateTime() {
+    const now = new Date();
+    const hours = now.getHours().toString().padStart(2, '0');
+    const minutes = now.getMinutes().toString().padStart(2,'0');
+    const seconds = now.getSeconds().toString().padStart(2,'0');
+    const timeString = `${hours}:${minutes}:${seconds}`;
+    document.getElementById('current-time').textContent = timeString;
+}
+// update time every second
+setInterval(updateTime, 1000);
+
+// Initial call to display the time immediately
+updateTime();
+
+
+let sidebar_navigation = document.getElementById('sidebar_navigation');
+
+let sidebar_navigation_bottom = document.getElementById('sidebar_buttom_menu');
+
+sidebar_navigation_bottom.addEventListener('click', function () {
+    if (sidebar_navigation.style.left == "" || sidebar_navigation.style.left == "-100%") {
+        sidebar_navigation.style.left = "0"
+        document.getElementById('sidebar_menu_icon').classList.add('fa-close');
+    }
+    else {
+        sidebar_navigation.style.left = "-100%"
+        document.getElementById('sidebar_menu_icon').classList.remove('fa-close');
+    }
+})
+
+
+let global_menu_buttom = document.getElementById('global_menu_button')
+let global_navigation = document.getElementById('global_menu_items')
+let global_close_buttom = document.getElementById('global_menu_close_button')
+
+global_menu_buttom.addEventListener('click', function () {
+    global_navigation.style.left = "0";
+})
+
+global_close_buttom.addEventListener('click', function () {
+    global_navigation.style.left = "-120%";
+})
